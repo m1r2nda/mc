48e5da226b95ae6420e113108367f798b8f0d918
Гладышева Екатерина <e.gla@skbkontur.ru>
2018-11-02 17:37:52 +0500

    +тренировочные задания

--- SeleniumTests/SeleniumTests/Training.cs
+++ SeleniumTests/SeleniumTests/Training.cs
@@ -24,14 +24,20 @@ namespace SeleniumTests
         {
             //Тренировочные локаторы
             driver.Navigate().GoToUrl("https://www.labirint.ru");
-            var books = driver.FindElements(By.ClassName("product-padding"));
-            var links = driver.FindElements(By.CssSelector("a.cover"));
-            var help = driver.FindElement(By.LinkText("Как сделать заказ"));
-            var logo = driver.FindElement(By.CssSelector("span.b-ymmain-title-e-name img"));
-            var menu = driver.FindElements(By.CssSelector(".b-header-b-menu-e-list li"));
-            var select = driver.FindElement(By.CssSelector("select[name='theme']"));
-            var option = driver.FindElement(By.CssSelector("select option[value='4']"));
-            var notDiscount = driver.FindElement(By.CssSelector("div.product-padding:not(.action-label__text)"));
+
+            var searchLine = By.Id("search-field"); //By.CssSelector("#search-field"); Найти локатор поля поиска, именно поиска, без кнопки Искать
+            var leftPresentCoupon = By.ClassName("form-getemail-checkout"); //By.CssSelector(".form-getemail-checkout");//найти локатор блока Вам подарок слева, где есть кнопка Получить купон
+            var getCoupon = By.ClassName("getemail-btn");
+            var aboutBook = By.CssSelector("#product-about"); //на странице конкретной книги блок Аннотации
+            var searchHelp = By.Name("searchformadvanced");//на странице https://www.labirint.ru/help/ найти форму поиска
+            var help = By.LinkText("Как сделать заказ");//в подвале
+            var partialText = By.PartialLinkText("Если ваша покупка дешевле номинала карты");//на странице https://www.labirint.ru/top/certificates/ найти текст
+            var allProducts =
+                By.XPath(
+                    "(//form[contains(@name, 'form-navisort')]//span[contains(@class, 'sorting-value menu-open')])[1]");//на странице рейтинги кнопка Все товары
+            var notDiscount = driver.FindElement(By.CssSelector("div.product-padding:not(.action-label__text)"));//книги без скидок
+            var year = By.CssSelector("select[name='year_begin'] >option[value='2017']");//на странице Вопрос ответ в селекте во вкладке Поиск в архиве найти 2017 год в начале периода поиска
+            var lefttext = By.CssSelector("a[class^=single-block]");//На странице https://www.labirint.ru/guestbook/ найти все ссылки-заголовки в левом блоке - где способы оплаты и тд
         }
 
         [TearDown]
