c5bf0bae7b0188fcdc5365592c0e79cbc73d6e7a
Гладышева Екатерина <e.gla@skbkontur.ru>
2018-11-06 13:34:39 +0500

    +добавили тестовый класс и сделли вспомогательные методы

--- SeleniumTests/SeleniumTests/FullTest.cs
+++ SeleniumTests/SeleniumTests/FullTest.cs
@@ -9,40 +9,23 @@ using OpenQA.Selenium.Support.UI;
 namespace SeleniumTests
 {
     [TestFixture]
-    public class FullTest
+    public class FullTest : SeleniumTestBase
     {
-        private IWebDriver driver;
-        private WebDriverWait wait;
-
-        [SetUp]
-        public void SetUp()
-        {
-            var options = new ChromeOptions();
-            options.AddArgument("--start-maximized"); // браузер раскрывается на весь экран
-            driver = new ChromeDriver(options);
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-        }
 
         [Test]
         public void MyFullTest()
         {
-            //Основной тест
             driver.Navigate().GoToUrl("https://www.labirint.ru");
+            AddBookInBasket();
+            RegOrder();
+            CourierDelivery();
+        }
 
-            //Локаторы
+        private void AddBookInBasket()
+        {
             var booksMenu = By.CssSelector("[data-toggle='header-genres']");
             var allBooks = By.CssSelector(".b-menu-second-container [href='/books/']");
             var basket = By.CssSelector(".product-padding a.buy-link");
-            var issue = By.CssSelector(".product-buy-margin a.buy-link");
-            var beginOrder = By.CssSelector("#basket-default-begin-order");
-            var courier = By.CssSelector("[data-gaid='cart_dlcourier']");
-            var city = By.CssSelector("input[data-suggeststype='district']");
-            var cityError = By.CssSelector("span.b-form-error");
-            var street = By.CssSelector(".js-dlform-wrap input[data-suggeststype='streets']");
-            var building = By.CssSelector(".js-dlform-wrap [name^=building]");
-            var flat = By.CssSelector(".js-dlform-wrap [name^=flat]");
-            var done = By.CssSelector(".js-dlform-wrap [value=Готово]");
 
             //Наводим на пункт меню либо через Actions либо через API http://jqueryui.com/menu/
             new Actions(driver)
@@ -58,58 +41,43 @@ namespace SeleniumTests
                 .Build()
                 .Perform();
 
-            //Кликаем у первой книги по кнопке "В корзину"
             driver.FindElement(basket).Click();
+        }
 
-            //Кликаем у первой книги по кнопке "Оформить"
-            //driver.FindElement(By.XPath("(//a[contains(@class,'btn')][contains(@class,'buy-link')][contains(@class,'btn-primary')][contains(@class,'btn-more')])[1]")).Click();
-            driver.FindElement(issue).Click();//по такому локатору все ок в режиме дебагера, потому что времени не хватает, 
-            //здесь можно добавить ожидалку, чтоб ее как раз пройти
+        private void RegOrder()
+        {
+            var issue = By.XPath(
+                "(//a[contains(@class,'btn')][contains(@class,'buy-link')][contains(@class,'btn-primary')][contains(@class,'btn-more')])[1]");
+            var beginOrder = By.CssSelector("#basket-default-begin-order");
 
-            //Кликаем по кнопке "Начать оформление"
+            driver.FindElement(issue).Click();
             driver.FindElement(beginOrder).Click();
+        }
+        
+        private void CourierDelivery()
+        {
+            var courier = By.CssSelector("[data-gaid='cart_dlcourier']");
+            var city = By.CssSelector("input[data-suggeststype='district']");
+            var cityError = By.CssSelector("span.b-form-error");
+            var street = By.CssSelector(".js-dlform-wrap input[data-suggeststype='streets']");
+            var building = By.CssSelector(".js-dlform-wrap [name^=building]");
+            var flat = By.CssSelector(".js-dlform-wrap [name^=flat]");
+            var done = By.CssSelector(".js-dlform-wrap [value=Готово]");
 
-            //Выбираем курьерскую доставку
             driver.FindElement(courier).Click();
-
-            //Вводим город некорректный
             driver.FindElement(city).SendKeys("saasdfsdfsdfdffds");
-
-            //Убираем фокус с поля, например, кликаем Tab
             driver.FindElement(city).SendKeys(Keys.Tab);
-
-            //Проверяем, что отобразилась ошибка
             Assert.IsTrue(driver.FindElement(cityError).Displayed, "Не появилась ошибка о неизвестном городе");
 
-            //Очищаем поле ввода и вводим город корректный
             driver.FindElement(city).Clear();
             driver.FindElement(city).SendKeys("Екатеринбург");
             driver.FindElement(city).SendKeys(Keys.Enter);
 
-            //Вводим название улицы
-
-            //Упадет так как поле неинтерактивное, так как их всего два таких на странице и первое поле не то
-            //driver.FindElement(By.CssSelector("input[data-suggeststype='streets']")).SendKeys("Ленина");
             driver.FindElement(street).SendKeys("Ленина");
-            
-            //Вводим номер дома
             driver.FindElement(building).SendKeys("1");
-
-            //Вводим номер квартиры
             driver.FindElement(flat).SendKeys("1");
-
-            //Указываем день доставки
             (driver as IJavaScriptExecutor).ExecuteScript($"$('.js-dlform-wrap .js-delivery-date').datepicker('setDate','{DateTime.Today.AddDays(8).ToString("dd.MM.yyyy")}')");
-
-            //И кликаем по кнопке "Готово"
             driver.FindElement(done).Click();
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            driver.Quit();
-            driver = null;
         }
     }
 }
---
+++ SeleniumTests/SeleniumTests/SeleniumTestBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public abstract class SeleniumTestBase
+    {
+        protected IWebDriver driver;
+        protected WebDriverWait wait;
+
+        [SetUp]
+        protected void SetUp()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized"); // браузер раскрывается на весь экран
+            driver = new ChromeDriver(options);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [TearDown]
+        protected void TearDown()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
